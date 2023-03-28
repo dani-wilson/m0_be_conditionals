ae7ea910129b70ee60b32aa3500d86e153584d15
Dani Wilson <danirae.powell@gmail.com>
2023-03-28 13:32:50 -0600

    Complete mild code challenge

---
+++ code_challenges/mild_code_challenge.rb
@@ -0,0 +1,17 @@
+## Mild
+
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, 
+# and the String "odd" if the Integer is odd.
+
+number = 7
+
+if number.even?
+    puts "That is an even number."
+else
+    puts "That's odd."
+end
+
+
+
+
