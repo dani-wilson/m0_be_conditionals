ea57100261b83b9a75f5175d9d53c26361027133
Dani Wilson <danirae.powell@gmail.com>
2023-03-27 20:28:34 -0600

    Add puts for my sanity

--- comparisons.rb
+++ comparisons.rb
@@ -21,36 +21,44 @@ numberDogs = 0
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
+puts "Is number_teachers less than number_students?"
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
 
 number_teachers < number_students
 
+puts "Is number_teachers equal to string_teachers?"
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
 
 number_teachers == string_teachers
 
+
+puts "Is number_teachers not equal to number_students?"
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
 number_teachers != number_students
 
+puts "Is number_students greater than or equal to 20?"
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
 
 number_students >= 20
 
+puts "is number_students greater than or equal to 21?"
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
 number_students >= 21
 
+puts "is number_students less than or equal to 20?"
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
 
 number_students <= 20
 
+puts "is number_students less than or equal to 21?"
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
 
